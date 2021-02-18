9b145d02371555f88072d1f622f63809bdb65876
TimOelrichs <toelris>
2021-02-18 12:05:13 +0100

    fixed login and added Escape Hatch

--- backend/server.js
+++ backend/server.js
@@ -63,8 +63,7 @@ const auth = () => {
     }
 }
 
-//app.post('/authenticate', auth() , (req, res) => {
-app.post('/authenticate', (req, res) => {
+app.post('/authenticate', auth() , (req, res) => {
     console.log("log in")
     res.status(200).json({"statusCode" : 200 ,"user" : req.user});
 });
